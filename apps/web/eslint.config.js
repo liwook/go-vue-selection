5cc1072911517ee91941bcc6df9fe0af51ef549c
lhj <abc@163.com>
2026-08-07 21:27:58 +0800

    build(web): add eslint and biome config

---
+++ apps/web/eslint.config.js
@@ -0,0 +1,25 @@
+import pluginVue from 'eslint-plugin-vue'
+import globals from 'globals'
+
+const vueRecommended = pluginVue.configs['flat/recommended']
+
+export default [
+  {
+    ignores: ['dist', 'node_modules', 'public', 'coverage'],
+  },
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      globals: { ...globals.browser },
+      parser: pluginVue.parser,
+    },
+  },
+  ...(Array.isArray(vueRecommended) ? vueRecommended : [vueRecommended]),
+  {
+    rules: {
+      'vue/multi-word-component-names': 'off',
+      'vue/html-self-closing': 'off',
+      'vue/max-attributes-per-line': 'off',
+    },
+  },
+]
